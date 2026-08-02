37705f66797c028a8b50cab36fd6e823beb16473
meghasharma123 <meghasharma2822000@gmail.com>
2026-08-02 14:00:13 +0530

    signup option added

--- src/Component/Login.tsx
+++ src/Component/Login.tsx
@@ -6,11 +6,14 @@ import { useNavigate } from "react-router-dom";
 import { BASE_URL } from "../util/constants";
 
 export default function Login() {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
   const [emailId, setEmailId] = useState("");
   const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [isLoginForm, setIsLoginForm] = useState(true);
   const [error, setError] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const handleLogin = async () => {
     try {
@@ -25,51 +28,111 @@ export default function Login() {
       dispatch(addUser(resp.data));
       navigate("/");
     } catch (err) {
-      const error = err as AxiosError
+      const error = err as AxiosError;
       if (error) {
         setError(error.response?.data as string);
-      }else{
+      } else {
         console.log("error login: ", error);
-        setError('Unknown error.')
+        setError("Unknown error.");
+      }
+    }
+  };
+
+  const handleSignUp = async () => {
+    try {
+      const res = await axios.post(
+        BASE_URL + "/signup",
+        { firstName, lastName, emailId, password },
+        { withCredentials: true },
+      );
+      dispatch(addUser(res.data.data));
+      return navigate("/profile");
+    } catch (err) {
+      const error = err as AxiosError;
+      if (error) {
+        setError(error.response?.data as string);
+      } else {
+        console.log("error login: ", error);
+        setError("Unknown error.");
       }
     }
   };
 
   return (
-    <div className="flex items-center justify-center h-full pt-5">
-      <div className="card w-96 bg-base-200 shadow-sm flex items-center justify-center">
+    <div className="flex justify-center my-10">
+      <div className="card bg-base-300 w-96 shadow-xl">
         <div className="card-body">
-          <div className="mt-4">
-            <div className="flex flex-col">
-              Email ID
+          <h2 className="card-title justify-center">
+            {isLoginForm ? "Login" : "Sign Up"}
+          </h2>
+          <div>
+            {!isLoginForm && (
+              <>
+                <label className="form-control w-full max-w-xs my-2">
+                  <div className="label">
+                    <span className="label-text">First Name</span>
+                  </div>
+                  <input
+                    type="text"
+                    value={firstName}
+                    className="input input-bordered w-full max-w-xs"
+                    onChange={(e) => setFirstName(e.target.value)}
+                  />
+                </label>
+                <label className="form-control w-full max-w-xs my-2">
+                  <div className="label">
+                    <span className="label-text">Last Name</span>
+                  </div>
+                  <input
+                    type="text"
+                    value={lastName}
+                    className="input input-bordered w-full max-w-xs"
+                    onChange={(e) => setLastName(e.target.value)}
+                  />
+                </label>
+              </>
+            )}
+            <label className="form-control w-full max-w-xs my-2">
+              <div className="label">
+                <span className="label-text">Email ID:</span>
+              </div>
               <input
-                value={emailId}
-                onChange={(e) => setEmailId(e.target.value)}
                 type="text"
-                placeholder="Email"
-                className="input w-70"
+                value={emailId}
+                className="input input-bordered w-full max-w-xs"
+                onChange={(e) => setEmailId(e.target.value)}
               />
-            </div>
-            <div className="mt-4 flex flex-col">
-              Password
+            </label>
+            <label className="form-control w-full max-w-xs my-2">
+              <div className="label">
+                <span className="label-text">Password</span>
+              </div>
               <input
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
                 type="password"
-                placeholder="Password"
-                className="input w-70"
+                value={password}
+                className="input input-bordered w-full max-w-xs"
+                onChange={(e) => setPassword(e.target.value)}
               />
-            </div>
+            </label>
           </div>
-          <div className="text-red-600">{error}</div>
-          <div className="mt-6">
+          <p className="text-red-500">{error}</p>
+          <div className="card-actions justify-center m-2">
             <button
-              onClick={() => handleLogin()}
-              className="btn btn-primary btn-block"
+              className="btn btn-primary"
+              onClick={isLoginForm ? handleLogin : handleSignUp}
             >
-              Login
+              {isLoginForm ? "Login" : "Sign Up"}
             </button>
           </div>
+
+          <p
+            className="m-auto cursor-pointer py-2"
+            onClick={() => setIsLoginForm((value) => !value)}
+          >
+            {isLoginForm
+              ? "New User? Signup Here"
+              : "Existing User? Login Here"}
+          </p>
         </div>
       </div>
     </div>
